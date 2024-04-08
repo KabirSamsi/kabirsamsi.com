1314805648645548002fdce1c54b13e426d94370
Kabir Samsi <samsikabir@gmail.com>
2024-04-08 16:09:30 -0400

    File config

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Test Message</h1>
+        <p>Initial Main Page Set Up</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
